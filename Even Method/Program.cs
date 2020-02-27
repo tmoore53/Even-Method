e837f2b643be796384c26d3644b9a1ce390f03fa
Tyler Moore <tyler.moore53@outlook.com>
2020-02-26 22:04:26 -0800

    Add project files.

---
+++ Even Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Even(626);
+            Even(763);
+
+        }
+        //Even Method takes in one integer perameter and retruns a response of it being even or odd.
+        static void Even(int input)
+        {
+            int output = input % 2;
+            if (output == 0)
+            {
+                Console.WriteLine($"{input} is a even number.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is a odd number.");
+            }
+
+
+        }
+    }
+}
